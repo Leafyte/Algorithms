68ac859cfeda70643877ea08daae8809edb296b8
Karthik M <karthikm12790@gmail.com>
2026-07-23 14:00:26 +0530

    Update AnagramFinder.java

--- Additional/AnagramFinder.java
+++ Additional/AnagramFinder.java
@@ -1,63 +1,23 @@
 import java.util.*;
 
 public class AnagramFinder {
-
-    public List<Integer> findAnagrams(String s, String p) {
-
-        List<Integer> result = new ArrayList<>();
-
-        if (s == null || p == null || s.length() < p.length()) {
-            return result;
-        }
-
-        int[] pCount = new int[26];
-        int[] sCount = new int[26];
-
-        for (int i = 0; i < p.length(); i++) {
-            pCount[p.charAt(i) - 'a']++;
-            sCount[s.charAt(i) - 'a']++;
-        }
-
-        if (Arrays.equals(pCount, sCount)) {
-            result.add(0);
-        }
-
-        for (int i = p.length(); i < s.length(); i++) {
-
-            sCount[s.charAt(i) - 'a']++;
-
-            sCount[s.charAt(i - p.length()) - 'a']--;
-
-            if (Arrays.equals(pCount, sCount)) {
-                result.add(i - p.length() + 1);
+    public static void main(String[] args) {
+        String s = "cbaebabacd";
+        String p = "abc";
+        
+        ArrayList<Integer> res = new ArrayList<>();
+        
+        for (int i = 0; i <= s.length() - p.length(); i++) {
+            String sub = s.substring(i, i + p.length());
+            char[] a = sub.toCharArray();
+            char[] b = p.toCharArray();
+            Arrays.sort(a);
+            Arrays.sort(b);
+            
+            if (Arrays.equals(a, b)) {
+                res.add(i);
             }
         }
-
-        return result;
-    }
-
-    public static void main(String[] args) {
-
-        AnagramFinder finder = new AnagramFinder();
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("--- Anagram Index Finder ---");
-
-        System.out.print("Enter the main string (s): ");
-        String s = scanner.nextLine();
-
-        System.out.print("Enter the pattern string (p): ");
-        String p = scanner.nextLine();
-
-        List<Integer> indices = finder.findAnagrams(s, p);
-
-        if (indices.isEmpty()) {
-            System.out.println("No anagrams found.");
-        } else {
-            System.out.println("Anagrams of \"" + p + "\" start at indices: " + indices);
-        }
-
-        scanner.close();
+        System.out.println(res);
     }
 }
